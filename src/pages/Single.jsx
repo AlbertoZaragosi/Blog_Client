9a07e9cb072740c1b7a476a92649cc3be0546bb6
AlbertoZaragosi <albertozartenorio@gmail.com>
2024-11-04 22:11:59 +0000

    Starting single page view front-end

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+        <div className="content"></div>
+        <div className="menu"></div>
+    </div>
   )
 }
 
